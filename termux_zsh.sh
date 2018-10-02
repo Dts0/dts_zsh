e973050df440670fe8e16b018bef84c3d6416ceb
DTS0 <dreamoftime0@gmail.com>
2018-10-02 22:47:38 +0800

    修改插件为从源地址git clone

--- termux_zsh.sh
+++ termux_zsh.sh
@@ -1,6 +1,12 @@
 apt install zsh -y
+apt install git -y
 
-cp .zshrc ~/.zshrc
+mkdir ~/.zsh
+cd ~./.zsh
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
-cp .zsh ~/.zsh
+cp -rf .zshrc ~/.zshrc
+
+chsh -s zsh
 
