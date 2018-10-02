7e9ca636d0cdd70386d2db213be1d2af5814a6fa
DTS0 <dreamoftime0@gmail.com>
2018-10-02 22:50:53 +0800

    修改一个小BUG,多写了一个.

--- termux_zsh.sh
+++ termux_zsh.sh
@@ -2,7 +2,7 @@ apt install zsh -y
 apt install git -y
 
 mkdir ~/.zsh
-cd ~./.zsh
+cd ~/.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
 
