42994d6aba23c8e6003d2071b62a27445b0a5ab5
DTS0 <dreamoftime0@gmail.com>
2018-10-02 22:54:42 +0800

    修改一个小BUG,调整了复制的顺序

--- termux_zsh.sh
+++ termux_zsh.sh
@@ -1,12 +1,11 @@
 apt install zsh -y
 apt install git -y
 
+cp -rf .zshrc ~/.zshrc
 mkdir ~/.zsh
 cd ~/.zsh
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 git clone https://github.com/zsh-users/zsh-autosuggestions.git
-
-cp -rf .zshrc ~/.zshrc
 
 chsh -s zsh
 
