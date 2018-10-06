5484bf3b705a971f1d034d4a4c4ae5f05c8fecdc
dts <33719983+Dts0@users.noreply.github.com>
2018-10-06 21:18:23 +0800

    增加python依赖

--- termux_vim.sh
+++ termux_vim.sh
@@ -2,6 +2,7 @@ apt install -y vim
 apt install -y git
 apt install -y ctags
 apt install -y curl
+apt install -y python
 
 curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
 source ~/.bashrc
