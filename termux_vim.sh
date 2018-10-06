ddabf6d69fa63b183f9cb1e1c00a2991e84ba41a
DTS0 <dreamoftime0@gmail.com>
2018-10-06 21:13:07 +0800

    增加vim安装脚本

---
+++ termux_vim.sh
@@ -0,0 +1,10 @@
+apt install -y vim
+apt install -y git
+apt install -y ctags
+apt install -y curl
+
+curl -sL https://raw.githubusercontent.com/egalpin/apt-vim/master/install.sh | sh
+source ~/.bashrc
+
+apt-vim install -y https://github.com/scrooloose/nerdtree.git
+
