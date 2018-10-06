eb7e6055941da523115d0c9fbb745046465583c8
dts <33719983+Dts0@users.noreply.github.com>
2018-10-06 20:58:30 +0800

    修改为curl获得安装脚本

--- termux_zsh.sh
+++ termux_zsh.sh
@@ -1,7 +1,8 @@
 apt install zsh -y
 apt install git -y
+apt install curl -y
 
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 cp -rf .zshrc ~/.zshrc
 mkdir ~/.zsh
