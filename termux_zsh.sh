4db987a94ef3ba7fda04ff4e2d126a7f24d9c874
dts <33719983+Dts0@users.noreply.github.com>
2018-10-06 20:54:02 +0800

    add oh-my-zsh

--- termux_zsh.sh
+++ termux_zsh.sh
@@ -1,6 +1,8 @@
 apt install zsh -y
 apt install git -y
 
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
 cp -rf .zshrc ~/.zshrc
 mkdir ~/.zsh
 cd ~/.zsh
